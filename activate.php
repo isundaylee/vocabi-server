5e8498773c66e3c2ed5cb6802c52b53daccb96c3
Ubuntu <ubuntu@ip-10-166-203-154.us-west-1.compute.internal>
2012-10-25 18:08:57 +0000

    Fixed syntax errors in activate.php.

--- activate.php
+++ activate.php
@@ -3,9 +3,9 @@
 $key = $_POST['key']; 
 
 if (file_exists('keys/' . $key)) {
-	echo json_encode({'result': TRUE}); 
+	echo json_encode(array('result' => TRUE)); 
 } else {
-	echo json_encode({'result': FALSE, 'error': 'Invalid key. '}); 
+	echo json_encode(array('result' => FALSE, 'error' => 'Invalid key. ')); 
 }
 
 ?>
