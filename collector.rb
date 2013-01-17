f11484b1802f262e7abaffac010b64cb2d18ece3
Sunday Lee <isundaylee.reg@gmail.com>
2013-01-17 22:44:03 +0800

    Added Barron List.

--- collector.rb
+++ collector.rb
@@ -1,25 +1,27 @@
 require 'rubygems'
 require 'json'
 
-nowl = 2
+count = 0
 total = 0
 
 wordlists = []
 
-1.upto(nowl) { |wlid|
-    open("wordlists/" + wlid.to_s + ".vdm", "r") { |f|
+Dir['wordlists/*.vdm'].each do |fn|
+  title = fn.match(/wordlists\/(.*).vdm/)[1]
+    open(fn, "r") { |f|
         data = f.read()
         result = JSON.parse(data)
-        wordlists += [{"title" => wlid.to_s, "words" => result}]; 
+        wordlists += [{"title" => title, "words" => result}]; 
         total += result.size
+        count += 1
     }
-}
+end
 
 open("collected.json", "w") { |f|
     f.write(wordlists.to_json)
 }
 
-puts "Concatenated " + nowl.to_s + " wordlists, in total " + total.to_s + " words. "
+puts "Concatenated " + count.to_s + " wordlists, in total " + total.to_s + " words. "
 
 version = open("version.php", "r").read().to_i
 version += 1
